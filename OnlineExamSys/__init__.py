db25c9c05d848b5bb486cda848eefbab20862826
hua691 <180472610@qq.com>
2026-04-22 22:07:16 +0800

    fix: optional pymysql import for cloud deploy

--- OnlineExamSys/__init__.py
+++ OnlineExamSys/__init__.py
@@ -1,2 +1,6 @@
-import pymysql
-pymysql.install_as_MySQLdb()
+# pymysql 只在本地 MySQL 环境需要;PA/Render/SQLite 环境没装就跳过
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
